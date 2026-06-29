bdcc16c51ea4174fa696e9f1e80170e4854c6e1b
Amanda Jorge <aatimoteojorge@gmail.com>
2026-06-29 16:14:37 -0300

    Delete 2022 directory

--- 2022/Hotel 2022.py
+++
@@ -1,9 +0,0 @@
-D = int(input()) # valor da diaria no dia 1
-A = int(input()) # valor do acrecimo
-N = int(input()) # dia da chegada
-if N <= 15:
-    valor = D + (N - 1) *A #Preço da diaria se tiver aumento
-else:
-    valor = D + 14 * A #Preço da diara se não tiver aumento
-total = valor * (31 - N + 1) #Preço total
-print(total)
